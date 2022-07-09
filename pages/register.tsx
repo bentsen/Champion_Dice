ae94de35ea169a8be2e8d1589225d2c54d406be4
Mikkel Bentsen <cph-mb781@cphbusiness.dk>
2022-07-09 16:41:53 +0200

    he

--- pages/register.tsx
+++ pages/register.tsx
@@ -75,7 +75,7 @@ const Register = () => {
                                             </div>
                                         </div>
                                         <div className={"mt-5"}>
-                                            <button onClick={handleSubmit} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-80 rounded">
+                                            <button onClick={} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-80 rounded">
                                                 Create Account
                                             </button>
                                         </div>
